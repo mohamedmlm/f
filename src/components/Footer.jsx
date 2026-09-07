8f0ea23ddf22e487e63180904a8361c9efe8f11d
mohamedmlm <mohamedmahmoodshabanplayer123@gmail.com>
2026-09-07 16:23:32 +0000

    Sep 7, 2026, 7:23 PM

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="container">
-        <span>© {new Date().getFullYear()} Shopify</span>
+        <span>© {new Date().getFullYear()} Crocs</span>
         <span className="text-faint"></span>
       </div>
     </footer>
