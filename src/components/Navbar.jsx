6e1d7f76b5e3fa9c20efdbfdbbd2e3591f800c2b
mohamedmlm <mohamedmahmoodshabanplayer123@gmail.com>
2026-09-09 19:20:28 +0000

    Sep 9, 2026, 10:20 PM

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -69,7 +69,7 @@ export default function Navbar() {
         <div
         ref={navBackdropRef}
         className="nav-backdrop"
-    aria-hidden={!menuOpen}
+        aria-hidden={!menuOpen}
   />
   )}
 
