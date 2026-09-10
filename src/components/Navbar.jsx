6a20f40d39e2e1b3a257f8488acd4a549263b5a8
mohamedmlm <mohamedmahmoodshabanplayer123@gmail.com>
2026-09-10 20:48:42 +0300

    Sep 10, 2026, 8:48 PM

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,6 +29,7 @@ export default function Navbar() {
     }
     return () => document.body.classList.remove("no-scroll");
   }, [menuOpen]);
+  
   useEffect(() => {
   if (isOpen) {
     document.body.classList.add("menu-open");
