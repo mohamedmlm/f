670f72f1091e7e32b0e266cdd24c874f2b3d3712
mohamedmlm <mohamedmahmoodshabanplayer123@gmail.com>
2026-09-10 22:13:27 +0300

    Sep 10, 2026, 10:13 PM

--- src/pages/admin/ItemForm.jsx
+++ src/pages/admin/ItemForm.jsx
@@ -86,7 +86,7 @@ export default function ItemForm() {
             <input type="number" required value={form.min} onChange={update("min")} />
           </div>
           <div className="field">
-            <label>  (max)</label>
+            <label> أصغر  (max)</label>
             <input type="number" required value={form.max} onChange={update("max")} />
           </div>
         </div>
