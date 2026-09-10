cf05c07fc1a02f896e9a0462997988b95e6ff23f
mohamedmlm <mohamedmahmoodshabanplayer123@gmail.com>
2026-09-10 18:09:46 +0000

    Sep 10, 2026, 9:09 PM

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,6 +30,15 @@ export default function Navbar() {
     }
     return () => document.body.classList.remove("menu-open");
   }, [isOpen]);
+  /* ================= Body scroll lock ================= */
+useEffect(() => {
+  if (isOpen) {
+    document.body.classList.add("menu-open");
+  } else {
+    document.body.classList.remove("menu-open");
+  }
+  return () => document.body.classList.remove("menu-open");
+}, [isOpen]);
 
   // ===== إغلاق عند تغيير المسار =====
   useEffect(() => {
