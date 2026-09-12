6459df67d60909051599b92483f58e709e9cfb4d
mohamedmlm <mohamedmahmoodshabanplayer@gmail.com>
2026-09-12 15:55:15 +0000

    Sep 12, 2026, 6:55 PM

--- src/pages/admin/ItemForm.jsx
+++ src/pages/admin/ItemForm.jsx
@@ -82,7 +82,7 @@ export default function ItemForm() {
         </div>
         <div className="field-row">
           <div className="field">
-            <label> جميع المقاسات موجودة من(min siz)</label>
+            <label> جميع المقاسات موجودة من(min size)</label>
             <input type="number" required value={form.min} onChange={update("min")} />
           </div>
           <div className="field">
