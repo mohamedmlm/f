46c66cd1f65283c8492c0180fb5492ea3a52c000
mohamedmlm <mohamedmahmoodshabanplayer@gmail.com>
2026-09-12 11:29:03 +0000

    Sep 12, 2026, 2:29 PM

--- src/pages/admin/ItemForm.jsx
+++ src/pages/admin/ItemForm.jsx
@@ -86,7 +86,7 @@ export default function ItemForm() {
             <input type="number" required value={form.min} onChange={update("min")} />
           </div>
           <div className="field">
-            <label> أكبر مقاس (max)</label>
+            <label>  مقأكبراس (max)</label>
             <input type="number" required value={form.max} onChange={update("max")} />
           </div>
         </div>
